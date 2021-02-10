d5c07d188a6bd543df8a41ab7ce075c1c5cfcf6a
Esau Brizuela Ruiz <esau.brizuela@outlook.com>
2021-02-10 17:55:40 -0600

    Commit Inicial de la creacion de Proyecto

---
+++ PAZCAL/src/pazcal/ArchivoInfo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pazcal;
+
+/**
+ *
+ * @author ebri_85
+ */
+public class ArchivoInfo {
+    
+}
