b007e83c6ac248d2556432fa7901cac851a3bee1
Esau Brizuela <ebri_85@outlook.com>
2021-03-28 15:04:05 -0600

    Se crea el metodo y esta funcional para validar el campo VAR

--- PAZCAL/src/pazcal/Valida.java
+++ PAZCAL/src/pazcal/Valida.java
@@ -55,9 +55,11 @@ public class Valida {
     private ArrayList<Integer> posicionTamanoLinea = new ArrayList<>();
     private ArrayList<Integer> posicionPuntoComa = new ArrayList<>();
     private ArrayList<Integer> posicionProgram = new ArrayList<>();
+    private ArrayList<Integer> posicionVar = new ArrayList<>();
+    private ArrayList<Integer> posicionDefVar = new ArrayList<>();
 
     protected CargaInformacion cargaInformacion;
-    protected boolean[] esProgramaValido = new boolean[5]; //cuando se genera un error que es de PAZCAL el valor debe de ser false para no compilar 
+    protected boolean[] esProgramaValido = new boolean[10]; //cuando se genera un error que es de PAZCAL el valor debe de ser false para no compilar 
     //y solo generar archivo errores y no llamar pascal.
 
     public Valida() {
@@ -84,6 +86,8 @@ public class Valida {
                 this.esProgramaValido[1] = lineasPuntoComa();
                 this.esProgramaValido[2] = EncuentraComentarios();
                 this.esProgramaValido[3] = ValidaProgram();
+                this.esProgramaValido[4] = ValidaVar();
+
                 evalua = false;
             }
 
@@ -115,6 +119,13 @@ public class Valida {
                             System.out.println("FORMATO PROGRAM........ [OK]");
                         } else {
                             System.out.println("FORMATO PROGRAM........ [ERROR]");
+                        }
+                        break;
+                    case 4:
+                        if (this.esProgramaValido[i]) {
+                            System.out.println("FORMATO VAR........ [OK]");
+                        } else {
+                            System.out.println("FORMATO VAR........ [ERROR]");
                         }
                         break;
 
@@ -233,26 +244,25 @@ public class Valida {
         }
     }
 
-    private int ValidaSuperfluos(String ln, int nLn) {
-        try {
-            int resultado;
-
-            String strInicio = "(\\s)?"; //<- deberia de validar si existe 2 o mas espacios en blanco
-            Pattern ptr = Pattern.compile(strInicio);
-
-            Matcher match = ptr.matcher(ln);
-
-            resultado = (match.matches()) ? 1 : 0;
-
-            return resultado;
-
-        } catch (Exception e) {
-            System.out.println("Clase Valida-> ValidaSuperfluos()=> " + e.getMessage());
-            e.printStackTrace();
-            return -1;
-        }
-    }
-
+//    private int ValidaSuperfluos(String ln, int nLn) {
+//        try {
+//            int resultado;
+//
+//            String strInicio = "(\\s)?"; //<- deberia de validar si existe 2 o mas espacios en blanco
+//            Pattern ptr = Pattern.compile(strInicio);
+//
+//            Matcher match = ptr.matcher(ln);
+//
+//            resultado = (match.matches()) ? 1 : 0;
+//
+//            return resultado;
+//
+//        } catch (Exception e) {
+//            System.out.println("Clase Valida-> ValidaSuperfluos()=> " + e.getMessage());
+//            e.printStackTrace();
+//            return -1;
+//        }
+//    }
     protected boolean EncuentraReservadas(String ln) {
         try {
             boolean resultado = false;
@@ -293,9 +303,8 @@ public class Valida {
 
             boolean continua = true;
             while (continua) {
-                //String patron =   "(([PROGRAM])(\\s+)([a-z]{14})(\\s*\\;{1}))|[PROGRAM]\\s+[a-z]{14}\\s+\\({1}\\s*[input]\\s*\\,{1}\\s*[output]\\s*\\){1}s*\\;{1}|[PROGRAM]\\s+[a-z]{14}\\s+\\(\\s*[input]\\s*\\)s*\\;{1}|[PROGRAM]\\s+[a-z]{14}\\s+\\(\\s*[\\s*output]\\s*\\)s*\\;{1}"; //PROGRAM NombreDePrograma ( INPUT, OUTPUT );
-                 //String patron = "(program)\\s+([a-z]{1,15})\\s+\\(\\s+(input)\\s+\\,{1}\\s+(output)\\s+\\)s+\\;{1}";
-                 String patron = "(program)\\s+[a-z]{1,15}\\s+\\({1}\\s+(input)\\s+\\,{1}\\s+(output)\\s+\\){1}\\s+\\;{1}|(program)\\s+[a-z]{1,15}\\s+\\;{1}|(program)\\s+[a-z]{1,15}\\s+\\({1}\\s+(input)\\s+\\){1}\\s+\\;{1}|(program)\\s+[a-z]{1,15}\\s+\\({1}\\s+(output)\\s+\\){1}\\s+\\;{1}";
+
+                String patron = "(program)\\s+[a-z]{1,15}\\s*\\({1}\\s*(input)\\s*\\,{1}\\s*(output)\\s*\\){1}\\s*\\;{1}|(program)\\s+[a-z]{1,15}\\s*\\;{1}|(program)\\s+[a-z]{1,15}\\s*\\({1}\\s*(input)\\s*\\){1}\\s*\\;{1}|(program)\\s+[a-z]{1,15}\\s*\\({1}\\s*(output)\\s*\\){1}\\s*\\;{1}";
                 Pattern ptr = Pattern.compile(patron, Pattern.CASE_INSENSITIVE);
 
                 this.cargaInformacion.codigoArchivo.forEach((String e) -> {
@@ -306,7 +315,7 @@ public class Valida {
                             boolean resultado = false;
                             Matcher mtch = ptr.matcher(e);
                             resultado = mtch.find();
-                            
+
                             if (!e.isEmpty()) {
                                 if (resultado == false) {
                                     this.posicionProgram.add(posicion);
@@ -325,6 +334,88 @@ public class Valida {
         } catch (Exception e) {
             System.out.println("Clase Valida-> ValidaProgram()=> " + e.getMessage());
             e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean ValidaVar() {
+        try {
+
+            String patron = "(VAR)";
+            String patron2 = "(BEGIN)";
+
+            Pattern ptr1 = Pattern.compile(patron, Pattern.CASE_INSENSITIVE);
+            Pattern ptr2 = Pattern.compile(patron2, Pattern.CASE_INSENSITIVE);
+
+            int posVar = 0;
+            int posBegin = 0;
+            ArrayList<String> temp = new ArrayList<>();
+            boolean resultado1 = true;
+            boolean resultado2 = true;
+            for (String e : this.cargaInformacion.codigoArchivo) {
+
+                int posicion = this.cargaInformacion.codigoArchivo.indexOf(e) + 1;
+
+                Matcher mtch1 = ptr1.matcher(e);
+                Matcher mtch2 = ptr2.matcher(e);
+
+                resultado1 = mtch1.matches();
+                resultado2 = mtch2.matches();
+
+                if (!e.isEmpty()) {
+                    if (resultado1) {
+                        posVar = this.cargaInformacion.codigoArchivo.indexOf(e);
+                    }
+
+                }
+                if (!e.isEmpty()) {
+                    if (resultado2) {
+                        posBegin = this.cargaInformacion.codigoArchivo.indexOf(e);
+                    }
+                }
+                mtch1.reset();
+            }
+            while (posVar < posBegin) {
+                temp.add(this.cargaInformacion.codigoArchivo.get(posVar));
+                posVar++;
+            }
+
+            for (String e : temp) {
+                boolean res = true;
+                String patronV = "(VAR)";
+                String patronVariables = "(\\s+(\\w[^\\.\\%]){1,15}:\\s+(INTEGER|CHAR|REAL);)";
+                Pattern ptrVariables = Pattern.compile(patronVariables, Pattern.CASE_INSENSITIVE);
+                Pattern ptrV = Pattern.compile(patronV, Pattern.CASE_INSENSITIVE);
+                
+                boolean esVariable = false;
+                
+                Matcher mtchV = ptrV.matcher(e);
+                Matcher mtchVariables = ptrVariables.matcher(e);
+                
+                res = mtchV.matches();
+                esVariable = mtchVariables.find();
+
+                if (!e.isEmpty()) {
+                    if (!res) {
+                        //System.out.println(res + "  " + e);
+                        this.posicionVar.add(this.cargaInformacion.codigoArchivo.indexOf(e) + 1);
+                    }
+                }
+                if (!e.isEmpty()) {
+                    if (esVariable) {
+                       // System.out.println(esVariable + "  " + e);
+                        this.posicionVar.add(this.cargaInformacion.codigoArchivo.indexOf(e) + 1);
+                    } 
+                }
+            }
+
+            Collections.sort(posicionVar);
+
+            return this.posicionVar.isEmpty();
+
+        } catch (Exception e) {
+            System.out.println("Clase Valida-> ValidaVar()=> " + e.getMessage());
+            e.getCause();
             return false;
         }
     }
@@ -459,6 +550,13 @@ public class Valida {
 
             this.posicionProgram.forEach((e) -> {
                 System.out.println("Linea Formato Program Erroneo-> " + e);
+            });
+            this.posicionVar.forEach((e) -> {
+                System.out.println("Linea Formato Var Erroneo-> " + e);
+            });
+
+            this.posicionDefVar.forEach((e) -> {
+                System.out.println("Linea Definicion Variable Error-> " + e);
             });
             imprimir = false;
         }
